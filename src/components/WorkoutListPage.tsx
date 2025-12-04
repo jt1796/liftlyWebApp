413745e0d5431060685b761e72e2bc1cbd7011ce
John Tompkins <tompkins.jt@gmail.com>
2025-12-03 22:53:28 -0500

    Add month divider on list page

--- src/components/WorkoutListPage.tsx
+++ src/components/WorkoutListPage.tsx
@@ -8,6 +8,7 @@ import {
   CircularProgress,
   Alert,
   ListItemButton,
+  Divider,
 } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { getWorkoutsForUser } from '../utils';
@@ -35,16 +36,45 @@ const WorkoutListPage: React.FC = () => {
       {error && <Alert severity="error">{(error as Error).message}</Alert>}
       {workouts && (
         <List>
-          {workouts.map((workout) => (
-            <ListItem key={workout.id} disablePadding>
-              <ListItemButton component={Link} to={`/workout/${workout.id}`}>
-                <ListItemText
-                  primary={workout.exercises.map((e) => e.name).join(', ')}
-                  secondary={workout.date.toLocaleDateString()}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {(() => {
+            let lastMonth: number | null = null;
+            return workouts.map((workout) => {
+              const currentMonth = workout.date.getMonth();
+              const monthChanged = lastMonth !== currentMonth;
+              lastMonth = currentMonth;
+              return (
+                <React.Fragment key={workout.id}>
+                  {monthChanged && (
+                    <React.Fragment>
+                      <Divider sx={{ my: 1 }} />
+                      <Typography
+                        variant="caption"
+                        sx={{ mt: 1, mb: 0.5, ml: 1, fontSize: '0.75rem' }}
+                      >
+                        {workout.date.toLocaleString('default', {
+                          month: 'long',
+                          year: 'numeric',
+                        })}
+                      </Typography>
+                    </React.Fragment>
+                  )}
+                  <ListItem disablePadding>
+                    <ListItemButton
+                      component={Link}
+                      to={`/workout/${workout.id}`}
+                    >
+                      <ListItemText
+                        primary={workout.exercises
+                          .map((e) => e.name)
+                          .join(', ')}
+                        secondary={workout.date.toLocaleDateString()}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                </React.Fragment>
+              );
+            });
+          })()}
         </List>
       )}
     </Container>
